874ab207dad7b91e6e238fdaa83798d4bb6844e0
zhaixj <15501053088@163.com>
2024-01-10 23:29:19 +0800

    尝试第四次提交

--- demo-module-1/src/main/java/com/duyi/TestMain.java
+++ demo-module-1/src/main/java/com/duyi/TestMain.java
@@ -4,6 +4,7 @@ public class TestMain {
     public static void main(String[] args) {
 
         System.out.println("代码测试");
+        System.out.println("这个时测试");
 
     }
 }
--- demo-module-1/src/main/java/com/duyi/TestMain1.java
+++ demo-module-1/src/main/java/com/duyi/TestMain1.java
@@ -4,5 +4,7 @@ public class TestMain1 {
     public static void main(String[] args) {
 
         System.out.println("Say Hello");
+        System.out.println("这个时另外一个修改");
+
     }
 }
