3bcac4727a20d2abc17d0c29ce610fbb98ff2d52
zhaixj <15501053088@163.com>
2024-01-11 14:42:44 +0800

    分支feature提交

--- demo-module-1/src/main/java/com/duyi/TestMain.java
+++ demo-module-1/src/main/java/com/duyi/TestMain.java
@@ -6,5 +6,6 @@ public class TestMain {
         System.out.println("代码测试");
         System.out.println("这个时测试");
         System.out.println("这个是功能分支");
+        System.out.println("分支:feature提交");
     }
 }
